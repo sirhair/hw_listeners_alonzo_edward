9aa072e7b7350fd6dec6ba0b3dd74e35d64c9eb3
Edward Alonzo <edward@thenewe.com>
2015-02-27 06:49:43 -0600

    add bgchange for autofocus

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
 //Change background of form input/textareas on blur and focus
-//change bg color on blur and focus
-//gets all input tags and assigns blur event listener
+//gets all input tags and textarea tags
 var inputArray = document.getElementsByTagName('input');
 var inputText = document.getElementsByTagName('textarea');
 
-//set blur and focus event listeners
+//set blur(off focus) and focus event listeners
 //for inputs
 for (i=0; i < inputArray.length; i++){
 	inputArray[i].addEventListener('blur', bgChange);
 	inputArray[i].addEventListener('focus', bgChange);
+	if (inputArray[i].autofocus ===true){
+		inputArray[i].style.backgroundColor="#dddddd";
+	}
 }
 
 //for textareas
 for (i=0; i < inputText.length; i++){
 	inputText[i].addEventListener('blur', bgChange);
 	inputText[i].addEventListener('focus', bgChange);
+	if (inputText[i].autofocus ===true){
+		inputText[i].style.backgroundColor="#dddddd";
+	}
 }
 
-
-//change background depending on event type
+//change bg color on blur(no longer focused) and on focus
 function bgChange(){
 	var eventInput= event.target;
 	console.log (event.type);
