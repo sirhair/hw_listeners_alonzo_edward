62b9e5011f0f0d15222cd5442eb0464f455cf22e
Edward Alonzo <edward@thenewe.com>
2015-02-27 05:13:29 -0600

    console log fields on submit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.getElementById("formsub").addEventListener("click", function(event){
+    event.preventDefault()
+    var firstName =document.getElementsByName("firstName")[0].value;
+    var lastName =document.getElementsByName("lastName")[0].value;
+    var email=document.getElementsByName("email")[0].value;
+    var message =document.getElementsByName("message")[0].value;
+
+
+    console.log("The firstName is: ", firstName);
+    console.log("The lastName is: ", lastName);
+    console.log("The email is: ", email);
+    console.log("The message is: ", message);
+
+    console.log("But it wasn't a dream. It was a place. And you and you and you...and you were there. But you couldn't have been could you?");
+
+
+});
