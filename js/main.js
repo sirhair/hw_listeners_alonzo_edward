aa4ae75d97b328df58f5ab6f94dd1fe379f6f64c
Edward Alonzo <edward@thenewe.com>
2015-02-27 06:37:24 -0600

    add blur/focus functionality to textareas

--- js/main.js
+++ js/main.js
@@ -2,14 +2,23 @@
 //change bg color on blur and focus
 //gets all input tags and assigns blur event listener
 var inputArray = document.getElementsByTagName('input');
+var inputText = document.getElementsByTagName('textarea');
 
 //set blur and focus event listeners
+//for inputs
 for (i=0; i < inputArray.length; i++){
 	inputArray[i].addEventListener('blur', bgChange);
 	inputArray[i].addEventListener('focus', bgChange);
 }
 
+//for textareas
+for (i=0; i < inputText.length; i++){
+	inputText[i].addEventListener('blur', bgChange);
+	inputText[i].addEventListener('focus', bgChange);
+}
 
+
+//change background depending on event type
 function bgChange(){
 	var eventInput= event.target;
 	console.log (event.type);
