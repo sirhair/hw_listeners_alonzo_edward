2eb3d8524c77af86f441c4c55f32c27380b7630a
Edward Alonzo <edward@thenewe.com>
2015-02-27 06:31:19 -0600

    change bgcolor of input on focus

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
+//Change background of form input/textareas on blur and focus
+//change bg color on blur and focus
+//gets all input tags and assigns blur event listener
 var inputArray = document.getElementsByTagName('input');
 
+//set blur and focus event listeners
 for (i=0; i < inputArray.length; i++){
 	inputArray[i].addEventListener('blur', bgChange);
+	inputArray[i].addEventListener('focus', bgChange);
 }
 
+
 function bgChange(){
-	var blurredInput= event.target;
-	blurredInput.style.backgroundColor="#888888";
-	console.log(blurredInput);
+	var eventInput= event.target;
+	console.log (event.type);
+	if (event.type =='blur'){
+		eventInput.style.backgroundColor="#888888";
+		console.log(eventInput);
+	} else if (event.type =='focus'){
+		eventInput.style.backgroundColor="#dddddd";
+		console.log(eventInput);
+		
+	}
+
 }
