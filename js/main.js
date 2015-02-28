a4923449b708480aaf59333478796f0ed0fbf81a
Edward Alonzo <edward@thenewe.com>
2015-02-28 00:37:02 -0600

    tweak button behaviour

--- js/main.js
+++ js/main.js
@@ -26,15 +26,37 @@ for (i=0; i < inputText.length; i++){
 function bgChange(){
 	var eventInput= event.target;
 	console.log (event.type);
-	if (event.type =='blur'){
-		eventInput.style.backgroundColor="#888888";
-		console.log(eventInput);
-	} else if (event.type =='focus'){
-		eventInput.style.backgroundColor="#dddddd";
-		console.log(eventInput);
-		
+	if ((event.target.id != 'formsub') && (event.target.id !='resetForm') && (event.target.id !='eraseForm')){
+		console.log(event.target.id);
+		if (event.type =='blur'){
+			eventInput.style.backgroundColor="#888888";
+			console.log(eventInput);
+		} else if (event.type =='focus'){
+			eventInput.style.backgroundColor="#dddddd";
+			console.log(eventInput);
+			
+		}
+	}else if((event.target.id =='resetForm') || (event.target.id =='eraseForm')){
+		// Brighten but not darken reset and erase buttons
+		if (event.type =='blur'){
+			eventInput.style.backgroundColor="#aaaaaa";
+			console.log(eventInput);
+		} else if (event.type =='focus'){
+			eventInput.style.backgroundColor="#dddddd";
+			console.log(eventInput);
+			
+		}
+	} else if (event.target.id == 'formsub'){
+		//brighten submit button and return to original color
+		if (event.type =='blur'){
+			eventInput.style.backgroundColor="#c894fc";
+			console.log(eventInput);
+		} else if (event.type =='focus'){
+			eventInput.style.backgroundColor="#e9b5ff";
+			console.log(eventInput);
+			
+		}
 	}
-
 }
 
 //erase form js, when erase button is clicked, erase the form from DOM
@@ -44,5 +66,5 @@ document.getElementById('eraseForm').addEventListener('click', eraseFunction);
 //erase form and replace with rr.com
 function eraseFunction(){
 	var eventInput= event.target;
-	eventInput.parentNode.innerHTML='<iframe width="640" height="480" src="https://www.youtube.com/embed/8qnQz8kxte0?start=18&autoplay=1" frameborder="0" allowfullscreen></iframe>';
+	eventInput.parentNode.innerHTML='<iframe src="https://www.youtube.com/embed/8qnQz8kxte0?start=18&autoplay=1" frameborder="0" allowfullscreen></iframe>';
 }
